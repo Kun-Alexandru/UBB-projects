885539a1472e9eedaa5701dfa6dd6c5876566dd0
Kun Alexandru <91133359+eXoDuSs8@users.noreply.github.com>
2023-05-05 13:15:54 +0300

    Add files via upload

---
+++ SEM4/DB/A1+A2/Lab2/Lab2/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.parentTable = new System.Windows.Forms.DataGridView();
+            this.childTable = new System.Windows.Forms.DataGridView();
+            this.insertButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.updateButton = new System.Windows.Forms.Button();
+            this.panel = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.parentTable)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.childTable)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // parentTable
+            // 
+            this.parentTable.AllowUserToAddRows = false;
+            this.parentTable.AllowUserToDeleteRows = false;
+            this.parentTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.parentTable.Location = new System.Drawing.Point(16, 27);
+            this.parentTable.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.parentTable.Name = "parentTable";
+            this.parentTable.ReadOnly = true;
+            this.parentTable.RowHeadersWidth = 51;
+            this.parentTable.Size = new System.Drawing.Size(783, 431);
+            this.parentTable.TabIndex = 0;
+            this.parentTable.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.parentTable_CellClick);
+            this.parentTable.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.parentTable_CellContentClick);
+            // 
+            // childTable
+            // 
+            this.childTable.AllowUserToAddRows = false;
+            this.childTable.AllowUserToDeleteRows = false;
+            this.childTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.childTable.Location = new System.Drawing.Point(843, 27);
+            this.childTable.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.childTable.Name = "childTable";
+            this.childTable.ReadOnly = true;
+            this.childTable.RowHeadersWidth = 51;
+            this.childTable.Size = new System.Drawing.Size(783, 431);
+            this.childTable.TabIndex = 1;
+            this.childTable.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.childTable_CellClick);
+            // 
+            // insertButton
+            // 
+            this.insertButton.Location = new System.Drawing.Point(52, 503);
+            this.insertButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.insertButton.Name = "insertButton";
+            this.insertButton.Size = new System.Drawing.Size(211, 42);
+            this.insertButton.TabIndex = 2;
+            this.insertButton.Text = "Insert";
+            this.insertButton.UseVisualStyleBackColor = true;
+            this.insertButton.Click += new System.EventHandler(this.insertButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(52, 591);
+            this.deleteButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(211, 42);
+            this.deleteButton.TabIndex = 3;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // updateButton
+            // 
+            this.updateButton.Location = new System.Drawing.Point(52, 686);
+            this.updateButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(211, 42);
+            this.updateButton.TabIndex = 4;
+            this.updateButton.Text = "Update";
+            this.updateButton.UseVisualStyleBackColor = true;
+            this.updateButton.Click += new System.EventHandler(this.updateButton_Click);
+            // 
+            // panel
+            // 
+            this.panel.Location = new System.Drawing.Point(327, 491);
+            this.panel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(1275, 250);
+            this.panel.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1641, 756);
+            this.Controls.Add(this.panel);
+            this.Controls.Add(this.updateButton);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.insertButton);
+            this.Controls.Add(this.childTable);
+            this.Controls.Add(this.parentTable);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.parentTable)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.childTable)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView parentTable;
+        private System.Windows.Forms.DataGridView childTable;
+        private System.Windows.Forms.Button insertButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button updateButton;
+        private System.Windows.Forms.Panel panel;
+    }
+}
+
