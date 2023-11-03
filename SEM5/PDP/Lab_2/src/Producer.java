a360d448976aeec6ac16543e8a6037d57279ad2e
Kun Alexandru <alexandru.kun@stud.ubbcluj.ro>
2023-11-03 21:10:26 +0200

    Added lab2 PDP

---
+++ SEM5/PDP/Lab_2/src/Producer.java
@@ -0,0 +1,32 @@
+import java.util.List;
+
+public class Producer extends Thread {
+
+    public ProducerConsumer producerConsumer;
+
+    public List<Integer> vector1, vector2;
+
+    public Producer(ProducerConsumer producerConsumer, List<Integer> vector1, List<Integer> vector2) {
+        super("P:");
+        this.producerConsumer = producerConsumer;
+        this.vector1 = vector1;
+        this.vector2 = vector2;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < vector1.size(); i++) {
+            try {
+                sleep(1000);
+                Integer val1 = vector1.get(i);
+                Integer val2 = vector2.get(i);
+                System.out.printf("P: Sending %d * %d = %d\n", val1, val2, val1 * val2);
+                producerConsumer.add(val1 * val2);
+
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+}
